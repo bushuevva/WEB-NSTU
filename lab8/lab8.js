46e2e6523ca237172ed9b671e083efbb77611246
Ирина Бушуева <bushueva.24@yandex.ru>
2024-05-31 22:03:16 +0700

    День недели

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -14,4 +14,18 @@ function showDate(){
     out5.innerHTML = 'Дата и время для локали Канаде: ' + today.toLocaleString('en-CA');
     out6.innerHTML = 'Дата и время для локали Испании: ' + today.toLocaleString('es-ES');
 
+    let year = document.querySelector('.year');
+    let month = document.querySelector('.month');
+    let date = document.querySelector('.date');
+    let week = document.querySelector('.week');
+
+    year.innerHTML = 'Текущий год: ' + today.getFullYear();
+    nowMonth = today.getMonth();
+    const MonthD = ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь', ]
+    month.innerHTML = 'Текущий месяц: ' + MonthD[nowMonth];
+    let nowDate = today.getDate();
+    date.innerHTML ='Текущая дата: ' +  nowDate;
+    nowWeek = today.getDay();
+    const WeekD = ['Воскресенье', 'Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
+    week.innerHTML = 'День недели: ' + WeekD[nowWeek];
 }
